fb164744afb1fac39d9836a7848e6c984f55a12e
AminAsa1 <139868668+AminAsa1@users.noreply.github.com>
2025-01-15 01:46:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ echo "Backhaul Script"
 echo
 sleep 1
 
+DIRECTORY="/etc/cluster"
+
 echo "Enter service name:"
 read serviceName
+
+if [ -d "$DIRECTORY" ]; then
+  echo "$DIRECTORY does exist."
+fi
