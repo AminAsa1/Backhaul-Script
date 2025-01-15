f156792f2996294eeb4707c486be71b75a022834
AminAsa1 <139868668+AminAsa1@users.noreply.github.com>
2025-01-15 04:28:54 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ echo
 sleep 1
 
 DIRECTORY="/etc/cluster"
+CHS=3
+serviceName="cluster"
 
-echo "Enter service name:"
-read serviceName
-
-echo "your service name is: $erviceName"
-
+#echo "Enter service name:"
+#echo "your service name is: $erviceName"
+backhaul_instller() {
 if [ -d "$DIRECTORY" ]; then
   echo "$DIRECTORY does exist."
 fi
@@ -21,7 +21,18 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 wget --inet4-only wget -P "$DIRECTORY" "https://github.com/Musixal/Backhaul/releases/latest/download/backhaul_linux_amd64.tar.gz"
+tar -xzf "$DIRECTORY/backhaul_linux_amd64.tar.gz" -C "$DIRECTORY"
+rm $DIRECTORY/LICENSE $DIRECTORY/README.md $DIRECTORY/backhaul_linux_amd64.tar.gz
+mv $DIRECTORY/backhaul $DIRECTORY/$serviceName
+}
 
-tar -xzf "$DIRECTORY/backhaul_linux_amd64.tar.gz"
-rm "$DIRECTORY/LICENSE $DIRECTORY/README.md $DIRECTORY/backhaul_linux_amd64.tar.gz"
-mv "backhaul $DIRECTORY/$serviceName"
+echo "Select Server Location:"
+echo "1.Iran"
+echo "2.kharej"
+echo "3.Exit"
+read -r -p "Select Number(Default is: 3):" CHS
+
+case $CHS in
+    1)  echo "Be carefull SSH port must under 23"
+    echo "Enter service name:"
+    read serviceName
